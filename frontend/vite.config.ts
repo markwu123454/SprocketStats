b1745ea0c216d4de5f7e1eb1539040ad5fce1e61
markwu123454 <139573640+markwu123454@users.noreply.github.com>
2026-01-12 15:23:19 -0800

    pray no bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
                             request.destination === "style",
                         handler: "StaleWhileRevalidate",
                         options: {
-                            cacheName: "assets-cache-v26.0.0",
+                            cacheName: "assets-cache-v26.0.2",
                         },
                     },
 
@@ -62,10 +62,10 @@ export default defineConfig({
                         urlPattern: ({request}) => request.destination === "image",
                         handler: "CacheFirst",
                         options: {
-                            cacheName: "images-cache-v26.0.0",
+                            cacheName: "images-cache-v26.0.2",
                             expiration: {
                                 maxEntries: 100,
-                                maxAgeSeconds: 60 * 60 * 24 * 30,
+                                maxAgeSeconds: 60 * 60 * 24 * 2,
                             },
                         },
                     },
@@ -74,7 +74,7 @@ export default defineConfig({
                         urlPattern: ({request}) => request.mode === "navigate",
                         handler: "NetworkFirst",
                         options: {
-                            cacheName: "html-cache-v26.0.1",
+                            cacheName: "html-cache-v26.0.2",
                             networkTimeoutSeconds: 10,
                             expiration: {
                                 maxEntries: 10,
